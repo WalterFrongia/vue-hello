659625ef97d033763cb36ca3ca123fd501f99d97
WalterFrongia <walter.frongia@libero.it>
2021-09-14 15:55:22 +0200

    Implementazione titolo con dati js

---
+++ myscript.js
@@ -0,0 +1,20 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+
+let app = new Vue(
+{
+    el: '#root',    
+    data: {         
+        title :"Titolo",
+    }
+});
+
+
+
+
+
+
+
+
+
+// Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
